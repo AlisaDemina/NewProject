9863c394c6ff35a767cd14be338a2c225773e773
Alisa Demina <AGVerhovod@gmail.com>
2021-04-13 14:23:34 +0300

    Новый сайт.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {Menu} from "./components/Menu";
+import {Main} from "./components/Main";
+import {Services} from "./components/Services";
+import {PortfolioGrid} from "./components/PortfolioGrid";
+import {About} from "./components/About";
+import {Team} from "./components/Team";
+import {Clients} from "./components/Clients";
+import {Contact} from "./components/Contact";
+import {Footer} from "./components/Footer";
+import {PortfolioModal1} from "./components/PortfolioModal1";
+import {PortfolioModal2} from "./components/PortfolioModal2";
+import {PortfolioModal3} from "./components/PortfolioModal3";
+import {PortfolioModal4} from "./components/PortfolioModal4";
+import {PortfolioModal5} from "./components/PortfolioModal5";
+import {PortfolioModal6} from "./components/PortfolioModal6";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Menu/>
+    <Main/>
+    <Services/>
+    <PortfolioGrid/>
+    <About/>
+    <Team/>
+    <Clients/>
+    <Contact/>
+    <Footer/>
+    <PortfolioModal1/>
+    <PortfolioModal2/>
+    <PortfolioModal3/>
+    <PortfolioModal4/>
+    <PortfolioModal5/>
+    <PortfolioModal6/>
     </div>
   );
 }
